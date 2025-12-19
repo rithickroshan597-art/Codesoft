007801f5bc55f7207abda7a15537ff1a48387358
rithickroshan597-art <rithickroshan597@gmail.com>
2025-12-19 22:55:42 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+# Task 2: Simple Calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\nChoose operation:")
+print("+  Addition")
+print("-  Subtraction")
+print("*  Multiplication")
+print("/  Division")
+
+operation = input("Enter operation: ")
+
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operation")
---
+++ todo_list.py
@@ -0,0 +1,53 @@
+
+
+# Task 1: To-Do List Application
+
+tasks = []
+
+def show_menu():
+    print("\n--- TO-DO LIST MENU ---")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Update Task")
+    print("4. Delete Task")
+    print("5. Exit")
+
+while True:
+    show_menu()
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter new task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == "2":
+        if not tasks:
+            print("No tasks found.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+    elif choice == "3":
+        task_no = int(input("Enter task number to update: "))
+        if 1 <= task_no <= len(tasks):
+            new_task = input("Enter new task: ")
+            tasks[task_no - 1] = new_task
+            print("Task updated!")
+        else:
+            print("Invalid task number.")
+
+    elif choice == "4":
+        task_no = int(input("Enter task number to delete: "))
+        if 1 <= task_no <= len(tasks):
+            tasks.pop(task_no - 1)
+            print("Task deleted!")
+        else:
+            print("Invalid task number.")
+
+    elif choice == "5":
+        print("Exiting To-Do List. Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
